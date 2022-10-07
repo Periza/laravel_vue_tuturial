968096e0794eb722cb257af52aa8adc0a8045ca6
Marko Perica <marko.perica2@gmail.com>
2022-10-07 19:32:13 +0200

    added tailwind

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-import store from './store'
-import './style.css'
+import './index.css'
 import App from './App.vue'
 
-createApp(App).use(store).mount('#app')
+createApp(App).mount('#app')
