af22e36fea71b48837a9192b63a24a4f67341bfb
Marko Perica <marko.perica2@gmail.com>
2022-10-18 07:19:29 +0200

    Login on backend

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -27,6 +28,34 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request) 
+    {
+        $credidentials = $request->validate([
+            'email' => 'requred|email|string|exists:users,email',
+            'password' => [
+                'required'
+            ],
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $credidentials['remember'] ?? false;
+        unset($credidentials['remember']);
+
+        if(!Auth::attempt($credidentials, $remember)) {
+            return response([
+                'error' => 'The Provided credentials are not correct'
+            ], 422);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken();
 
         return response([
             'user' => $user,
