9db04c8629ba7404cb435068749f43ab6858e6c7
Marko Perica <marko.perica2@gmail.com>
2022-10-20 07:57:41 +0200

    create survey pages

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,9 +3,10 @@ import DefaultLayout from '../components/DefaultLayout.vue';
 import store from "../store";
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue';
-import Register from '../views/Register.vue'
-import Surveys from '../views/Surveys.vue'
-import AuthLayout from '../components/AuthLayout.vue'
+import Register from '../views/Register.vue';
+import Surveys from '../views/Surveys.vue';
+import SurveyView from '../views/SurveyView.vue';
+import AuthLayout from '../components/AuthLayout.vue';
 
 const routes = [
     {
@@ -23,6 +24,16 @@ const routes = [
                 path: '/surveys',
                 name: 'Surveys',
                 component: Surveys
+            },
+            {
+                path:"surveys/create",
+                name: "SurveyCreate",
+                component: SurveyView
+            },
+            {
+                path:"surveys/:id", 
+                name:"SurveyView",
+                component: SurveyView
             }
         ]
     },
