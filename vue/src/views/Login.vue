263cdc410ffe1f0225f2fbf77dd3583c38b5dfe9
Marko Perica <marko.perica2@gmail.com>
2023-01-05 07:53:17 +0100

    show validation errors

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -9,14 +9,14 @@
           </p>
         </div>
         <form class="mt-8 space-y-6" @submit="login">
-          <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
-            {{errorMsg}}
+          <Alert v-if="errorMsg">
+            {{ errorMsg }}
             <span @click="errorMsg = ''" class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </span>
-          </div>
+          </Alert>
           <input type="hidden" name="remember" value="true" />
           <div class="-space-y-px rounded-md shadow-sm">
             <div>
@@ -37,10 +37,14 @@
           </div>
   
           <div>
-            <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <button :class="{'cursor-not-allowed' : loading, 'hover:bg-indigo-500': loading }" :disabled="loading" type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
               <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                 <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
               </span>
+              <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
               Sign in
             </button>
           </div>
@@ -49,6 +53,7 @@
   
 <script setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
+import Alert from '../components/Alert.vue'
 import store from '../store'
 import {useRouter} from "vue-router";
 import {ref} from "vue";
@@ -56,6 +61,8 @@ import {ref} from "vue";
 let errorMsg = ref('');
 
 const router = useRouter();
+
+const loading = ref(false);
 
 const user = {
   email: '',
@@ -66,6 +73,8 @@ const user = {
 function login(ev) {
   ev.preventDefault();
 
+  loading.value = true;
+
   store
     .dispatch("login", user)
     .then(() => {
@@ -75,6 +84,9 @@ function login(ev) {
     })
     .catch((err) => {
       errorMsg.value = err.response.data.error
+    })
+    .finally(() => {
+      loading.value = false;
     });
 }
 </script>
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -9,7 +9,15 @@
       </p>
     </div>
     <form class="mt-8 space-y-6" @submit="register">
-      <input type="hidden" name="remember" value="true" />
+
+      <Alert v-if="Object.keys(errors).length" class="flex-col items-stretch text-sm">
+        <div v-for="(field, i) of Object.keys(errors)" :key="i">
+          <div v-for="(error, ind) of errors[field] || []" :key="ind" >
+            * {{ error }}
+          </div>
+        </div>
+      </Alert>
+
       <div class="-space-y-px rounded-md shadow-sm">
         <div>
           <label for="fullname" class="sr-only">Name</label>
@@ -30,10 +38,20 @@
       </div>
 
       <div>
-        <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <button 
+        type="submit" 
+        class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        :disabled="loading"
+        :class="{'cursor-not-allowed' : loading, 'hover:bg-indigo-500': loading }"
+        >
           <span class="absolute inset-y-0 left-0 flex items-center pl-3">
             <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
           </span>
+
+          <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
           Sign up
         </button>
       </div>
@@ -44,6 +62,8 @@
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 import store from '../store'
 import {useRouter} from "vue-router"
+import {ref} from "vue";
+import Alert from '../components/Alert.vue';
 
 const router = useRouter();
 
@@ -54,13 +74,25 @@ const user = {
   password_confirmation: ''
 }
 
+const loading = ref(false);
+
+const errors = ref({});
+
 function register(ev) {
   ev.preventDefault();
+  loading.value = true;
   store.dispatch('register', user)
     .then((res) => {
       router.push({
         name: 'Dashboard'
       });
-    });
+    })
+    .catch(err => {
+      if(err.response.status === 422)
+        errors.value = err.response.data.errors;
+    })
+    .finally(() => {
+      loading.value = false;
+    })
 }
 </script>
